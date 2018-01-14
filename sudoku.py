9f3e050fcd26b85902467906a06d604de81007ee
Alexey Zaytsev <alex.rv9rh@gmail.com>
2018-01-14 21:13:25 +0500

    filled detect_square(), cheked

--- sudoku.py
+++ sudoku.py
@@ -1,3 +1,47 @@
 
 def detect_square(string, column):
-	pass
+	x1 = 0
+	y1 = 0
+	x2 = 0
+	y2 = 0
+
+	string = int(string)
+	column = int(column)
+
+	if string <= 2:
+		y1 = 0
+		y2 = 2
+	
+	elif string > 2 and string <= 5:
+		y1 = 3
+		y2 = 5
+	
+	elif string > 5:
+		y1 = 6
+		y2 = 8
+
+	if column <= 2:
+		x1 = 0
+		x2 = 2
+
+	elif column > 2 and column <= 5:
+		x1 = 3
+		x2 = 5
+
+	elif column > 5:
+		x1 = 6
+		x2 = 8
+
+	return (x1, y1, x2, y2)
+
+
+while True:
+	x = input(':')
+	if x == 'q':
+		break
+
+	x = list(x.split())
+
+	print(detect_square(x[0], x[1]))
+
+	
