7179f11108832a68b244d54e5ff02f45955e9187
Alexey Zaytsev <alex.rv9rh@gmail.com>
2018-01-15 21:56:07 +0500

    add 'game_field', 'create_unique_set()';

--- sudoku.py
+++ sudoku.py
@@ -1,4 +1,16 @@
 
+game_field = [
+[1, 2, 3, 2, 0, 0, 5, 0, 0],
+[4, 5, 6, 0, 0, 0, 0, 0, 0],
+[7, 8, 9, 0, 0, 0, 0, 0, 0],
+[4, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 3, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0],
+[0, 0, 0, 0, 0, 0, 0, 0, 0],
+[2, 0, 0, 0, 0, 0, 0, 5, 0]]
+
+
 def detect_square(string, column):
 	x1 = 0
 	y1 = 0
@@ -34,4 +46,38 @@ def detect_square(string, column):
 
 	return (x1, y1, x2, y2)
 
+def create_unique_set(string, column):
+	all_numbers = set(range(1, 10))
+	exist_numbers = set()
 
+	our_square = detect_square(string, column)
+	x1 = our_square[0]
+	y1 = our_square[1]
+	x2 = our_square[2] + 1
+	y2 = our_square[3] + 1
+
+	for i in range(0, 9):
+		x = game_field[string][i]
+		if x != 0:
+			exist_numbers.add(x)
+
+		y = game_field[i][column]
+		if y != 0:
+			exist_numbers.add(y)
+
+	for i in range(y1, y2):
+		for j in range(x1, x2):
+			n = game_field[i][j]
+			if n != 0:
+				exist_numbers.add(n)
+
+	unique_set = all_numbers - exist_numbers
+
+	# return all_numbers, exist_numbers, unique_set
+	return unique_set
+
+print(create_unique_set(0, 0))
+# print(create_unique_set(3, 3))
+# print(create_unique_set(8, 8))
+print(create_unique_set(3, 7))
+# print(set(range(0, 9)))
